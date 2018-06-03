c1e7e08583c1623fd1d2b24c92e29248b0ac03e0
01speed1 <o1speed1@hotmail.com>
2018-06-02 23:44:03 -0500

    x

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ let config = require('./config/data');
 
 // dowload apk
 app.get('/download', (sol, res) => {
-    res.download( __dirname +'/downloads/ej.apk', 'easy-jobs-1.0-.apk' )
+    res.download( __dirname +'/downloads/ej.apk', 'easy-jobs-1.0-.apk', (err)=>{
+        if (err) res.redirect('/')
+        else res.json({ok:true})
+    })
 
     
 })
