053b03ea274093038dac4fd4d846f308f871ae18
01speed1 <o1speed1@hotmail.com>
2018-06-02 23:41:31 -0500

    x

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ let app = express();
 // config
 let config = require('./config/data');
 
+// dowload apk
+app.get('/download', (sol, res) => {
+    res.download( __dirname +'/downloads/ej.apk', 'easy-jobs-1.0-.apk' )
+
+    
+})
+
 require('./config/express')(app, express, config);
 
 
